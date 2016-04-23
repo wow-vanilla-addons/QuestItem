8096ba766bc79161118ca6fbc0355a3ca160a25d
Jozef Izso <jozef.izso@gmail.com>
2016-04-23 11:11:02 +0200

    QuestItem 1.0.1

--- QuestItem/localization.lua
+++ QuestItem/localization.lua
@@ -2,7 +2,7 @@
 Default localization for QuestItem
 ]]--
 
-QUESTITEM_VERSION 				= "1.0";
+QUESTITEM_VERSION 				= "1.0.1";
 QUESTITEM_NAME 					= "QuestItem";
 QUESTITEM_TITLE 				= "Shagoth's " .. QUESTITEM_NAME;
 
